769e68f0260c3e76cb5188133f1fd128ada6e63c
pranay15155 <pranayprasoon15@gmail.com>
2024-06-29 06:23:59 +0530

    adding user profile

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,20 @@ import "dotenv/config";
 import mongoose from "mongoose";
 
 import myUserRoute from "./routes/MyUserRoute";
-// import { v2 as cloudinary } from "cloudinary";
-// import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 // import restaurantRoute from "./routes/RestaurantRoute";
 // import orderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 const app = express();
 app.use(express.json()); //middleware
@@ -22,7 +28,7 @@ app.get("/health",async(req:Request,res:Response)=>{
 })
 
 app.use("/api/my/user", myUserRoute);
-// app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 // app.use("/api/restaurant", restaurantRoute);
 // app.use("/api/order", orderRoute);
 
