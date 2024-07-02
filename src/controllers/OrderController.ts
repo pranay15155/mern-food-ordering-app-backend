fafc119d7306437d07ba74d94596ab9f9831ac4a
pranay15155 <pranayprasoon15@gmail.com>
2024-07-02 21:45:24 +0530

    order feature

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -7,18 +7,18 @@ const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
 const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
 
-// const getMyOrders = async (req: Request, res: Response) => {
-//   try {
-//     const orders = await Order.find({ user: req.userId })
-//       .populate("restaurant")
-//       .populate("user");
+const getMyOrders = async (req: Request, res: Response) => {
+  try {
+    const orders = await Order.find({ user: req.userId })
+      .populate("restaurant")
+      .populate("user");
 
-//     res.json(orders);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "something went wrong" });
-//   }
-// };
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+};
 
 type CheckoutSessionRequest = {
   cartItems: {
@@ -67,12 +67,6 @@ const stripeWebhookHandler = async (req: Request, res: Response) => {
 
   res.status(200).send();
 
-  // console.log("recieved event");
-  // console.log("========");
-  // console.log("event:",req.body);
-  // res.send();
-  
-  
   
 };
 
@@ -186,7 +180,7 @@ const createSession = async (
 };
 
 export default {
-//   getMyOrders,
+  getMyOrders,
   createCheckoutSession,
   stripeWebhookHandler,
 };
